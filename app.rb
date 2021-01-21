a4578fbffcd164e2640a97303d9fe303ae22e6cc
chrislemus <icrislemus@gmail.com>
2021-01-21 10:32:12 -0500

    commit tree 6a0d59a0194d52b90e6f08d2d2f8f397a5cccf82

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+  end 
+
+  get('/') {
+    erb :index
+  }
+
+  post('/checkout') {
+    session["item"] =  params['item']
+    @session = session
+  }
+
 end
